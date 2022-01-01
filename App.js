957abd844ef53962be6d79a3b1f2e7e8496f8e75
wahyuanggara25 <wahyuanggara583@gmail.com>
2022-01-02 03:09:40 +0700

    AplikasiPertama

---
+++ App.js
@@ -0,0 +1,36 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from "react";
+import {Text, View} from 'react-native';
+
+
+const App = () => {
+return (
+  <View>
+    <View style={{width: 80, height: 80, backgroundColor: '#0abde3'}}/>
+      <Text>PRAWIOT</Text>
+      <Hudoro/>
+      <Text>asnjnakj</Text>
+      <Text>sfsdfa</Text>
+
+  </View>
+);
+
+};
+
+const Hudoro  = () =>{
+return <Text>Hudoro nmandasjdaksd</Text>;
+};
+
+const Photo = () =>{
+
+}
+
+
+export default App;
